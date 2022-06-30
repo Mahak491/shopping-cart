2e67e076a7c711f0e43a92b75a993733d7497bcb
Mahak491 <shreemahak01@gmail.com>
2022-06-30 17:02:42 +0530

    calci func update

--- main.js
+++ main.js
@@ -48,3 +48,26 @@ let increment = (id) => {
   update(selectedItem.id);
   localStorage.setItem("data", JSON.stringify(basket));
 };
+let decrement = (id) => {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+  
+    if (search === undefined) return;
+    else if (search.item === 0) return;
+    else {
+      search.item -= 1;
+    }
+    update(selectedItem.id);
+    basket = basket.filter((x) => x.item !== 0);
+    localStorage.setItem("data", JSON.stringify(basket));
+  };
+  let update = (id) => {
+    let search = basket.find((x) => x.id === id);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+  };
+  
+  let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+  };
